ab2b8a6518f74e0e7c6d7b6081e17af656e71acd
jcook997 <88389036+jcook997@users.noreply.github.com>
2021-12-08 22:05:29 -0500

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,15 @@
+function Todo({ todo, index, remove }) {
+  function handle() {
+    console.log("Ping:", index);
+    remove(index);
+  }
+  return (
+    <div className="todo" onClick={handle}>
+      {todo.text}{" "}
+      <a href="#!" data-mdb-toggle="tooltip" title="Remove">
+        {" "}
+        <i class="fas fa-trash-alt"></i>{" "}
+      </a>{" "}
+    </div>
+  );
+}
